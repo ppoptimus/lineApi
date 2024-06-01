92ab217d5ce03c89c28dd45235a683fcd107218f
rathapong <rathapong@promptnow.com>
2024-06-01 15:56:16 +0700

    add cors

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
-const https = require("https")
-const express = require("express")
-const app = express()
-const PORT = process.env.PORT || 3000
+const https = require("https");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const app = express();
+const PORT = process.env.PORT || 3000;
 const LINE_ACCESS_TOKEN = process.env.LINE_ACCESS_TOKEN;
 
-app.use(express.json())
-app.use(express.urlencoded({
-  extended: true
-}))
+app.use(cors());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/", (req, res) => {
-  res.send('สวัสดี express')
-})
+  res.send("สวัสดี express");
+});
 
 app.post("/webhook", (req, res) => {
-    console.log('req.body =>', JSON.stringify(req.body,null,2))
-    res.send("HTTP POST request sent to the webhook URL!")
-  })
+  const { userId, message } = req.body;
+  axios
+    .post(
+      "https://api.line.me/v2/bot/message/push",
+      {
+        to: userId,
+        messages: [
+          {
+            type: "text",
+            text: message,
+          },
+        ],
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${LINE_ACCESS_TOKEN}`,
+        },
+      }
+    )
+    .then((response) => {
+      res.status(200).json({ success: true });
+    })
+    .catch((error) => {
+      console.error("Error sending message:", error);
+      res.status(500).json({ success: false, error });
+    });
+//   res.send("HTTP POST request sent to the webhook URL!");
+});
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
-})
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
